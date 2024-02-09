fd3e00fd4772ebe137cb21796af192828017b005
Radhika Gupta <guptaradhika78@gmail.com>
2024-02-08 18:29:21 -0800

    added iterable checks to the pull data func

--- CalTrans/api.py
+++ CalTrans/api.py
@@ -56,7 +56,22 @@ class API():
         else:
             print("error")
     
+    """
+    Pulls data for multiple station IDs over specified time ranges. This function supports both single and multiple time ranges.
+    
+    If `start_times` and `end_times` are provided as single values, these times will be applied to all station IDs uniformly. 
+    If they are provided as iterables (lists or tuples), each station ID will be associated with the corresponding start and end time from these iterables. 
+    The function checks for consistency in the lengths of the station_ids, start_times, and end_times lists. If the lengths do not match, an error message is printed, and the function exits without pulling data.
+        
+    Note:
+    - Ensure that start_times and end_times are either both single values or both iterables of the same length as station_ids.
+    - The function does not return any value. It is designed to perform data pulling operations through the `pull_data(parameters)` method.
+    """
     def pull_data_for_multiple_stations(self, station_ids, start_times, end_times):
+        if not hasattr(start_times, '__iter__') or not hasattr(end_times, '__iter__'):
+            start_times = [start_times] * len(station_ids)
+            end_times = [end_times] * len(station_ids)
+
         if len(station_ids) != len(start_times) != len(end_times):
             print("Error: Length of station_ids, start_times, and end_times must be the same.")
             return
@@ -66,6 +81,5 @@ class API():
             self.pull_data(parameters)
              
 
-# generate_query(station_id=station_id, start_time=start_time, end_time=end_time)
 
 
--- example.py
+++ example.py
@@ -8,10 +8,7 @@ def main():
     start_times = [1706659200]
     end_times = [1706705940]
 
-    # Pull data for each station
-    # for station_id, start_time, end_time in zip(station_ids, start_times, end_times):
-    #     parameters = api.generate_query(station_id=station_id, start_time=start_time, end_time=end_time)
-    #     api.pull_data(parameters)
+
     api.pull_data_for_multiple_stations(station_ids, start_times, end_times)
 
 if __name__ == "__main__":
